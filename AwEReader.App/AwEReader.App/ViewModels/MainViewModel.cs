d9313b8effedb01765c113f8354b756fad9e54e8
Carl <carlst99@outlook.co.nz>
2023-09-05 23:56:14 +1200

    Add app files

---
+++ AwEReader.App/AwEReader.App/ViewModels/MainViewModel.cs
@@ -0,0 +1,6 @@
+﻿namespace AwEReader.App.ViewModels;
+
+public class MainViewModel : ViewModelBase
+{
+    public string Greeting => "Welcome to Avalonia!";
+}
---
+++ AwEReader.App/AwEReader.App/ViewModels/ViewModelBase.cs
@@ -0,0 +1,7 @@
+﻿using ReactiveUI;
+
+namespace AwEReader.App.ViewModels;
+
+public class ViewModelBase : ReactiveObject
+{
+}
